fe1925379ea9d0e878e9ff53260d6a9a6e88c554
kkk-sh25 <yangagkrisha@gmail.com>
2024-09-03 20:34:24 +1200

    bass

--- music_starter.js
+++ music_starter.js
@@ -19,29 +19,64 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   triangle(triDrum, height/1.58, triDrum - width/10, height/1.6, width/4, bothat+height/2); // lower triangle
 
   strokeWeight(1.5) 
-  line(width/6.5, height/1.7,width/6.5,height/1.3);//stand
+  line(width/7, height/1.7,width/6.5,height/1.3);//stand
   line(width/11, height/1.28,width/6.5,height/1.3);//feet1
   line(width/9, height/1.23,width/6.5,height/1.3);//feet2
   line(width/5, height/1.28,width/6.5,height/1.3);//feet3
 
   //snare
+  let snare = map(bass,0,100,100,110)
   fill(148, 85, 53)
   strokeWeight(0)
-  rect(width/3.2, height/1.37, 100, 57);
+  rect(width/3.6, height/1.37, 100, snare-48);
   strokeWeight(1.5)
-  line(width/3.2, height/1.31,width/3.2,height/1.13);//stand
-  line(width/3.2, height/1.13,width/4,height/1.1);//feet1
-  line(width/3.2, height/1.13,width/3,height/1.1);//feet2
-  line(width/3.2, height/1.13,width/2.7,height/1.1);//feet3
+  stroke('black')
+  line(width/3.6, height/1.31,width/3.6,height/1.13);//stand
+  line(width/3.6, height/1.13,width/4,height/1.1);//feet1
+  line(width/3.6, height/1.13,width/3.2,height/1.1);//feet2
+  line(width/3.6, height/1.13,width/2.7,height/1.1);//feet3
   for(var i = 1; i < 20; i+=5){
     let drawcircle = i*5;
-    line(drawcircle+width/3.9,height/1.39,drawcircle+width/3.9,height/1.31);
+    line(drawcircle+width/4.5,height/1.39,drawcircle+width/4.5,snare+height/1.57);
   
   }
+  
   strokeWeight(0)
-  fill(225)
-  ellipse(width/3.2,height/1.43,100,45)//head
+  fill(0)
+  ellipse(width/3.6,height/1.43,snare-5,45)//shadow
 
+  fill(225)
+  ellipse(width/3.6,height/1.45,snare-5,45)//head
+
+  let stick = map(bass,0,100,100,145)
+  strokeWeight(3.5)
+  line(width/3.8, stick+height/1.85,width/5,stick+height/1.68);//drum stick 1
+  
+  let head = map(bass,0,100,100,115)
+  strokeWeight(0)
+  fill(0)
+  circle(width/3.8, stick+height/1.85, 10)
+
+  //bass drum
+  strokeWeight(20)
+  stroke('black')
+  line(width/2.35,height/1.23,width/2.5,height/1.15)
+  line(width/1.75,height/1.23,width/1.65,height/1.15)
+  strokeWeight(0)
+  fill(205, 92, 8)
+  circle(width/2,height/1.41,200,215)//ouside
+  strokeWeight(5)
+  stroke(120, 116, 116)
+  fill(225)
+  circle(width/2,height/1.35,200)//inside
+
+  strokeWeight(1)
+  stroke('black')
+  for(var i = 1; i < 20; i+=5){
+    let drawcircle = i*5;
+    line(drawcircle+width/2.4,height/1.39,drawcircle+width/2,snare+height/1.57);
+  
+  }
   // // //square()
   // fill(70)
   // circle(100, height-200, 1000)
